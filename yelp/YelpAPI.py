f48fe32fe5c3044bd633bbc77d2399f95e37c2a0
Jian Qin <qinjianxyz@Jian-Chin.local>
2021-06-18 19:09:39 -0700

    update

--- yelp/YelpAPI.py
+++ yelp/YelpAPI.py
@@ -2,6 +2,7 @@
 
 def get_my_key():
     x = '4XqQqRXEU0mIi6G4hys7c-_ejBk5CrdCG6S8kRq3U7YYD30KjeQxaGRvsIrfCpYg2ES2EMpMOObuj4oxJJHKKLo-J-TGqOKiA7yp20GLu6MOhbyL2OWrLre_TySXYHYx'
+    y = 'n58swccfexfy-icHgke_5ch3437VMrYsUhSQ8gtup7QKJaSnMPD9YS4zyZRt0DbwRxVlgsOoMTvXuAjvDmc4wBnM72h0qsv_GGss8CWVxIdbIG7xZvHbcr3QBAeqYHYx'
     return x
 
 
